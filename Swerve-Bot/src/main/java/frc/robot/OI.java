ae384b4c25bec17c4397727e0d4c77d9e7cdc82a
Zandar-Double07 <91700064+Zandar-Double07@users.noreply.github.com>
2023-11-07 18:11:31 -0600

    oi change

--- Swerve-Bot/src/main/java/frc/robot/OI.java
+++ Swerve-Bot/src/main/java/frc/robot/OI.java
@@ -3,7 +3,23 @@ package frc.robot;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class OI {
-    XboxController driveController = new XboxController(0);
-    
+
+    private static XboxController driveController = new XboxController(0);
+
+    public static double getLeftX(){
+        return driveController.getLeftX();
+    }
+
+    public static double getLeftY(){
+        return driveController.getRightX();
+    }
+
+    public static double getRightX(){
+        return driveController.getRightX();
+    }
+
+    public static double getRightY(){
+        return driveController.getRightY();
+    }
 
 }
