bd887491e1bb360840facef9d6b3c2d69d7ec56a
Zandar-Double07 <91700064+Zandar-Double07@users.noreply.github.com>
2024-02-25 19:31:53 -0600

    update constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,7 +25,7 @@ import edu.wpi.first.math.util.Units;
  */
 public final class Constants {
   public static final Mode currentMode = Mode.REAL;
-  public static final double ANGLE_START_POSITION = -2;
+  public static final double ANGLE_START_POSITION = -.75;
   public static final double ANGLE_CLIMB_UP = 100;
   public static final double ANGLE_CLIMB_DOWN = 10;
   public static final double ANGLE_AMP = 100;
