a024305375275eb5927177be6f71cebd78153262
Zandar-Double07 <91700064+Zandar-Double07@users.noreply.github.com>
2024-03-05 20:39:59 -0600

    Invert Heading error to fix heading pid controller

--- src/main/java/frc/robot/AprilTags/AprilTagLock.java
+++ src/main/java/frc/robot/AprilTags/AprilTagLock.java
@@ -3,24 +3,28 @@ package frc.robot.AprilTags;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import org.littletonrobotics.junction.Logger;
 
 public class AprilTagLock implements RotationSource {
 
   NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  private double PIDResult;
   public static PIDController rotationPID = createPIDController();
 
   private static PIDController createPIDController() {
-    PIDController pid = new PIDController(.01, .02, .001);
+    PIDController pid = new PIDController(.02, 0.01, 0.001);
     pid.setTolerance(.25); // allowable angle error
     pid.enableContinuousInput(
-        0, 360); // it is faster to go 1 degree from 359 to 0 instead of 359 degrees
+        -180, 180); // it is faster to go 1 degree from 359 to 0 instead of 359 degrees
     pid.setSetpoint(0); // 0 = apriltag angle
     return pid;
   }
 
   @Override
   public double getR(double Heading) {
-    return rotationPID.calculate(Heading);
+    PIDResult = rotationPID.calculate(-Heading);
+    Logger.recordOutput("Drive/R", PIDResult);
+    return PIDResult;
 
     // return rotationPID.calculate(table.getEntry("tx").getDouble(0));
 
