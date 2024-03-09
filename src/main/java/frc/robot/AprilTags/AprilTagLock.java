be2977d17a8718c17ab1abede3186e5fc89600f8
Zandar-Double07 <91700064+Zandar-Double07@users.noreply.github.com>
2024-03-09 09:39:04 -0600

    Update AprilTagLock.java

--- src/main/java/frc/robot/AprilTags/AprilTagLock.java
+++ src/main/java/frc/robot/AprilTags/AprilTagLock.java
@@ -3,6 +3,7 @@ package frc.robot.AprilTags;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.littletonrobotics.junction.Logger;
 
 public class AprilTagLock implements RotationSource {
@@ -12,8 +13,8 @@ public class AprilTagLock implements RotationSource {
   public static PIDController rotationPID = createPIDController();
 
   private static PIDController createPIDController() {
-    PIDController pid = new PIDController(1.25, 0, 0);
-    pid.setTolerance(.25); // allowable angle error
+    PIDController pid = new PIDController(0.05, 0, 0);
+    pid.setTolerance(1); // allowable angle error
     pid.enableContinuousInput(
         -180, 180); // it is faster to go 1 degree from 359 to 0 instead of 359 degrees
     pid.setSetpoint(0); // 0 = apriltag angle
@@ -22,11 +23,11 @@ public class AprilTagLock implements RotationSource {
 
   @Override
   public double getR(double Heading) {
-    PIDResult = rotationPID.calculate(-Heading);
+    // PIDResult = rotationPID.calculate(-Heading);
     Logger.recordOutput("Drive/R", PIDResult);
-    return PIDResult;
+    SmartDashboard.putNumber("Heading", Heading);
+    // return PIDResult;
 
-    // return rotationPID.calculate(table.getEntry("tx").getDouble(0));
-
+    return -rotationPID.calculate(table.getEntry("tx").getDouble(0));
   }
 }
