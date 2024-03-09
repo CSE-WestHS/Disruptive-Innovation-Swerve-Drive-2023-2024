9e3f02ef1e23b29b8be72bb366eeb1e2cdf61fc3
Zandar-Double07 <91700064+Zandar-Double07@users.noreply.github.com>
2024-03-09 08:13:02 -0600

    april tag

--- src/main/java/frc/robot/AprilTags/AprilTagLock.java
+++ src/main/java/frc/robot/AprilTags/AprilTagLock.java
@@ -12,7 +12,7 @@ public class AprilTagLock implements RotationSource {
   public static PIDController rotationPID = createPIDController();
 
   private static PIDController createPIDController() {
-    PIDController pid = new PIDController(1.25, 0, 0);
+    PIDController pid = new PIDController(0.75, .01, 0);
     pid.setTolerance(.25); // allowable angle error
     pid.enableContinuousInput(
         -180, 180); // it is faster to go 1 degree from 359 to 0 instead of 359 degrees
@@ -22,7 +22,7 @@ public class AprilTagLock implements RotationSource {
 
   @Override
   public double getR(double Heading) {
-    PIDResult = rotationPID.calculate(-Heading);
+    PIDResult = rotationPID.calculate(-table.getEntry("tx").getDouble(0)));
     Logger.recordOutput("Drive/R", PIDResult);
     return PIDResult;
 
