7a601252edb924f968fecab03087e4655eb21bf9
Zandar-Double07 <91700064+Zandar-Double07@users.noreply.github.com>
2024-03-09 10:34:28 -0600

    json dump test

--- src/main/java/frc/robot/AprilTags/AprilTagLock.java
+++ src/main/java/frc/robot/AprilTags/AprilTagLock.java
@@ -1,20 +1,21 @@
 package frc.robot.AprilTags;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.LimelightHelpers;
+
 import org.littletonrobotics.junction.Logger;
 
 public class AprilTagLock implements RotationSource {
-
+  LimelightHelpers limelight = new LimelightHelpers();
   NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
   private double PIDResult;
   public static PIDController rotationPID = createPIDController();
-
+  
   private static PIDController createPIDController() {
-    PIDController pid = new PIDController(0.05, 0, 0);
-    pid.setTolerance(1); // allowable angle error
+    PIDController pid = new PIDController(0.75, .01, 0);
+    pid.setTolerance(.25); // allowable angle error
     pid.enableContinuousInput(
         -180, 180); // it is faster to go 1 degree from 359 to 0 instead of 359 degrees
     pid.setSetpoint(0); // 0 = apriltag angle
@@ -23,11 +24,17 @@ public class AprilTagLock implements RotationSource {
 
   @Override
   public double getR(double Heading) {
-    // PIDResult = rotationPID.calculate(-Heading);
+    PIDResult = rotationPID.calculate(-table.getEntry("tx").getDouble(0));
     Logger.recordOutput("Drive/R", PIDResult);
-    SmartDashboard.putNumber("Heading", Heading);
-    // return PIDResult;
+    String dump = limelight.getJSONDump("limelight");
+    LimelightHelpers.LimelightResults llresults = LimelightHelpers.getLatestResults("limelight");
+    LimelightHelpers.LimelightTarget_Fiducial[] fiducials = llresults.targetingResults.targets_Fiducials;
+    SmartDashboard.putString("llresults", dump);
+    
+    return PIDResult;
 
-    return -rotationPID.calculate(table.getEntry("tx").getDouble(0));
+    // return rotationPID.calculate(table.getEntry("tx").getDouble(0));
+
   }
 }
+
