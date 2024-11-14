32bf3a58bf40f3585cfb3f84c593e0ade3c320e0
6317DS2 <162824217+6317DS2@users.noreply.github.com>
2024-11-14 17:56:37 -0600

    help me

--- src/main/java/frc/robot/subsystems/ohiocamera/testTrack.java
+++ src/main/java/frc/robot/subsystems/ohiocamera/testTrack.java
@@ -4,7 +4,17 @@
 
 package frc.robot.subsystems.ohiocamera;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.MjpegServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
+
+
+
 /** Add your docs here. */
 public class testTrack {
-    
+   UsbCamera usbCamera = new UsbCamera("USB Camera 0",0); 
+   MjpegServer mjpegServer1 = new MjpegServer("serve_USB Camera 0", 1181);
+   
 }
